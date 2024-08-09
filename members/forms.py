2d76b918f1bd4a113b0cced5d74cce1e32c13ce1
Paulo Figueiredo <figueiredo@linkmetrik.com>
2024-08-09 17:00:50 -0300

    Alexandre (#4)
    * Campos obrigatórios alterados no forms
    
    * Criação 3 novos campos(box de como conheceu)
    
    * Campos novos do forms rodando
    
    ---------
    
    Co-authored-by: alexandre-222 <alexandre@linkmetrik.com>

--- members/forms.py
+++ members/forms.py
@@ -1,29 +1,41 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}), label='E-mail *', label_suffix='')
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome *', label_suffix='')
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}), label='Sobrenome *', label_suffix='')
-    city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}), label='Cidade *', label_suffix='')
-    fone = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}), label='Telefone *', label_suffix='')
-    instagram = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}), required=False, label='Instagram')
-    birth = forms.DateField(widget=forms.DateInput(attrs={'class':'form-control', 'type': 'date'}), label='Data de nascimento *', label_suffix='')
-    job_title = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':'form-control'}), label='Profissão *', help_text='Ex.: Programador Backend Java.', label_suffix='')
+    COMO_CONHECEU_CHOICES = [
+        ('instagram', 'Instagram'),
+        ('facebook', 'Facebook'),
+        ('twitter', 'Twitter'),
+        ('telegram', 'Telegram'),
+        ('amigos', 'Amigos'),
+        ('youtube', 'YouTube'),
+        ('indicacao', 'Indicação'),
+        ('outros', 'Outros')
+    ]
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), label='E-mail *')
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Nome *')
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Sobrenome *')
+    city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Cidade *')
+    fone = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Telefone *')
+    instagram = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False, label='Instagram')
+    birth = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), label='Data de nascimento *')
+    job_title = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Profissão *', help_text='Ex.: Programador Backend Java.')
     lightining_wallet = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Carteira Lightining', required=False)
     bsc_wallet = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Carteira BSC', required=False)
+    
+    como_conheceu = forms.ChoiceField(choices=COMO_CONHECEU_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}), label='Como conheceu o Fraguismo? *')
+    quem_indicou = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'display:none;'}), required=False, label='Quem indicou?')
+    aonde = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'display:none;'}), required=False, label='Outros? Aonde?')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'city', 'fone', 'instagram', 'birth', 'job_title')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'city', 'fone', 'instagram', 'birth', 'job_title', 'lightining_wallet', 'bsc_wallet', 'como_conheceu', 'quem_indicou', 'aonde')
 
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].label_suffix = ' *'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].label_suffix = ' *'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].label_suffix = ' *'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.label_suffix = ''
+
