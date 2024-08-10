bb61fecf43421bce5468c4baaf4a4c2c6056e262
alexandre-222 <alexandre@linkmetrik.com>
2024-08-10 16:47:58 -0300

    Criando a tela de profiles em admin

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nivel', 'pontuacao', 'observacao', 'pendencias')
+    search_fields = ('user__username', 'nivel')
+
+admin.site.register(Profile, ProfileAdmin)
--- members/models.py
+++ members/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    NIVEL_CHOICES = [
+        ('membro', 'Membro'),
+        ('assessor', 'Assessor'),
+        ('execultivo', 'Executivo'),
+        ('lider', 'Líder'),
+        ('diretor', 'Diretor'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nivel = models.CharField(max_length=10, choices=NIVEL_CHOICES, default='membro')
+    pontuacao = models.IntegerField(default=0)  # Novo campo de pontuação
+    observacao = models.TextField(blank=True)   # Novo campo de observação
+    pendencias = models.TextField(blank=True)   # Novo campo de pendências
+
+    def __str__(self):
+        return self.user.username
