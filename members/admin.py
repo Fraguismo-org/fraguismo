dafe226ceef1173a0b0fa2f4d248e77d93626da5
alexandre-222 <alexandre@linkmetrik.com>
2024-08-10 17:30:41 -0300

    Adicionando o campo de Squad

--- members/admin.py
+++ members/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'nivel', 'pontuacao', 'observacao', 'pendencias')
-    search_fields = ('user__username', 'nivel')
+    list_display = ('user', 'nivel', 'pontuacao', 'observacao', 'pendencias', 'squad')  # Adicione 'squad' aqui
+    search_fields = ('user__username', 'nivel', 'squad')  # Adicione 'squad' nas opções de pesquisa
 
 admin.site.register(Profile, ProfileAdmin)
--- members/models.py
+++ members/models.py
@@ -9,12 +9,30 @@ class Profile(models.Model):
         ('lider', 'Líder'),
         ('diretor', 'Diretor'),
     ]
-    
+
+    SQUAD_CHOICES = [
+        ('squad_021', 'Squad 021'),
+        ('area_51', 'Area 51'),
+        ('curitiba', 'Curitiba'),
+        ('serra_gaucha', 'Serra Gaucha'),
+        ('squad_011', 'Squad 011'),
+        ('squad_pampa', 'Squad Pampa'),
+        ('squad_061', 'Squad 061'),
+        ('squad_063', 'Squad 063'),
+        ('squad_064', 'Squad 064'),
+        ('squad_065', 'Squad 065'),
+        ('squad_066', 'Squad 066'),
+        ('squad_067', 'Squad 067'),
+        ('squad_069', 'Squad 069'),
+        ('squad_serra', 'Squad Serra'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nivel = models.CharField(max_length=10, choices=NIVEL_CHOICES, default='membro')
-    pontuacao = models.IntegerField(default=0)  # Novo campo de pontuação
-    observacao = models.TextField(blank=True)   # Novo campo de observação
-    pendencias = models.TextField(blank=True)   # Novo campo de pendências
+    pontuacao = models.IntegerField(default=0)
+    observacao = models.TextField(blank=True)
+    pendencias = models.TextField(blank=True)
+    squad = models.CharField(max_length=20, choices=SQUAD_CHOICES, default='squad_021')  # Novo campo Squad
 
     def __str__(self):
         return self.user.username
