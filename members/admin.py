2f3417523d2ced587ecb59c4f81e8602f113254b
Paulo Figueiredo <figueiredo@linkmetrik.com>
2024-08-11 10:54:55 -0300

    Alexandre3 (#5)
    * Criando a tela de profiles em admin
    
    * Adicionando o campo de Squad
    
    ---------
    
    Co-authored-by: alexandre-222 <alexandre@linkmetrik.com>

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nivel', 'pontuacao', 'observacao', 'pendencias', 'squad')  # Adicione 'squad' aqui
+    search_fields = ('user__username', 'nivel', 'squad')  # Adicione 'squad' nas opções de pesquisa
+
+admin.site.register(Profile, ProfileAdmin)
