aa171aaab31189613761b86cc04208dc7882cb3c
Figueiredo <figueiredo@linkmetrik.com>
2024-08-18 18:19:07 -0300

    corrigido link para a page password-reset-complete

--- members/urls.py
+++ members/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('password-reset/', PasswordResetView.as_view(template_name='authenticate/password-reset.html'), name='password-reset'),
     path('password-reset-done/', PasswordResetDoneView.as_view(template_name='authenticate/password-reset-done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='authenticate/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='authenticate/password-reset-complete.html'), name='password-reset-complete'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='authenticate/password-reset-complete.html'), name='password_reset_complete'),
 ]
