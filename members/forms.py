019ba727ca62c5077d4e192259a9c08dca676764
sanditomaz <sandi.contato@gmail.com>
2024-08-18 23:36:16 -0300

    features: form features implemented

--- members/forms.py
+++ members/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
 class RegisterUserForm(UserCreationForm):
     COMO_CONHECEU_CHOICES = [
         ('instagram', 'Instagram'),
@@ -26,15 +25,32 @@ class RegisterUserForm(UserCreationForm):
     bsc_wallet = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Carteira BSC', required=False)    
     como_conheceu = forms.ChoiceField(choices=COMO_CONHECEU_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}), label='Como conheceu o Fraguismo? *')
     quem_indicou = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'display:none;'}), required=False, label='Quem indicou?')
-    aonde = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'display:none;'}), required=False, label='Outros? Aonde?')
+    aonde = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'display:none;'}), required=False, label='Outros?')
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'city', 'fone', 'instagram', 'birth', 'job_title', 'lightining_wallet', 'bsc_wallet', 'como_conheceu', 'quem_indicou', 'aonde')
 
     def __init__(self, *args, **kwargs):
-        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        super(RegisterUserForm, self).__init__(*args, **kwargs)              
+        self.fields['password1'].help_text = ('<ul class="password-instructions">''<li class="password-requirement">Sua senha não pode ser muito parecida com o resto das suas informações pessoais.</li><li class="password-requirement">Sua senha precisa conter pelo menos 8 caracteres.</li><li class="password-requirement">Sua senha não pode ser uma senha comumente utilizada.</li><li class="password-requirement">Sua senha não pode ser inteiramente numérica.</li>''</ul>') 
+        self.fields['password1'].widget.attrs.update({'class': 'password-field'})
+
+        como_conheceu = self.data.get('como_conheceu')
+        if como_conheceu == 'indicacao':
+            self.fields['quem_indicou'].label = 'Quem indicou?'
+        else:
+            self.fields['quem_indicou'].label = ''
+ 
+        if como_conheceu == 'outros':
+            self.fields['aonde'].label = 'Onde conheceu?'
+        else:
+            self.fields['aonde'].label = ''
+     
+        
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.label_suffix = ''
 
+  
+ 
