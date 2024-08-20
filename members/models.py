bc51fcbc90b6f2ce7b621aa190202159372b1af9
Figueiredo <figueiredo@linkmetrik.com>
2024-08-20 18:57:04 -0300

    Adicionado members_user

--- members/models.py
+++ members/models.py
@@ -36,3 +36,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class User(User):
+    city = models.CharField(max_length=100)
+    fone = models.CharField(max_length=30)
+    instagram = models.CharField(max_length=100, blank=True)
+    birth = models.DateField()
+    job_title = models.CharField(max_length=255)
+    bsc_wallet = models.CharField(max_length=100, blank=True)    
+    como_conheceu = models.CharField(max_length=20)
+    quem_indicou = models.CharField(max_length=100, blank=True)
+    aonde = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return super().__str__()
+    
