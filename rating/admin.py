750b006ce1eaad2c4939421041aace1918ee225c
alexandre-222 <alexandre@linkmetrik.com>
2024-08-27 23:01:09 -0300

    Pontuação Dinamica

--- rating/admin.py
+++ rating/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import LogRating
+from members.models import Profile  # Certifique-se de importar o modelo Profile
 
 class LogRatingAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'pontuacao', 'pontuacao_ganha', 'nome_atividade', 'updated_at')
+    list_display = ('user_id','pontuacao','pontuacao_ganha', 'nome_atividade', 'updated_at')
     search_fields = ('user_id__username', 'nome_atividade')
+    readonly_fields = ('profile_pontuacao',)
+
+    def profile_pontuacao(self, obj):
+        profile = Profile.objects.get(user=obj.user_id)
+        return profile.pontuacao
+
+    profile_pontuacao.short_description = 'Pontuação do Profile' 
 
 admin.site.register(LogRating, LogRatingAdmin)
-
-
-
-
-
-
--- rating/models.py
+++ rating/models.py
@@ -1,17 +1,41 @@
 from datetime import datetime
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from members.models import Profile, User
+
 
 
 class LogRating(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    pontuacao = models.IntegerField(default=0)
     pontuacao_ganha = models.IntegerField(default=0)
+    pontuacao = models.IntegerField(default=0)
     nome_atividade = models.CharField(max_length=50, blank=True)
-    updated_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    @property
+    def pontuacao(self):
+        profile = Profile.objects.get(user=self.user_id)
+        return profile.pontuacao
+    def save(self, *args, **kwargs):
+        # Obtenha o perfil do usuário
+        profile = Profile.objects.get(user=self.user_id)
 
-    def __str__(self) -> str:
-        return f'{self.user_id} | pontuacao total: {self.pontuacao + self.pontuacao_ganha} | {self.nome_atividade} | {self.updated_at}'
+        # Armazene a pontuação atual antes de atualizar
+        self.pontuacao_antes = profile.pontuacao
+        
+        # Salva o log normalmente, incluindo a pontuação antes da atualização
+        super().save(*args, **kwargs)
+        
+        # Atualiza a pontuação do perfil
+        profile.pontuacao += self.pontuacao_ganha
+        profile.save()
+    
+    
+
+
+def __str__(self) -> str:
+        local_time = timezone.localtime(self.updated_at)
+        return f'{self.user_id} | pontuacao total: {self.pontuacao + self.pontuacao_ganha} | {self.nome_atividade} | {local_time}'
 
 
