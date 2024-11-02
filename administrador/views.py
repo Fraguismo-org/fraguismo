1640a17e8d5cc49d3dd4c14f0fb370116509a9da
Paulo Figueiredo <figueiredo@linkmetrik.com>
2024-11-02 17:37:05 -0300

    Correção pontuação

--- administrador/views.py
+++ administrador/views.py
@@ -20,16 +20,25 @@ def lista_usuarios(request):
 
 @user_passes_test(lambda u: u.is_superuser)
 def pendencia_usuario(request, username):
+    if request.method == 'POST':
+        p_pendencia_id = request.POST.get('profile_pendencia_id')
+        p_pendencia = ProfilePendencia.objects.get(id=p_pendencia_id)
+        p_pendencia.pendencia_status = 2
+        p_pendencia.save()
     user = Users.objects.get(username=username)
     profile = Profile.get_or_create_profile(user)
     nivel = Nivel.objects.get(nivel=profile.nivel.lower())
     nivel = nivel.proximo_nivel()
     pendencias = Pendencia.objects.filter(nivel=nivel)
-    profile_pendencias = ProfilePendencia.objects.filter(profile=profile)
+    profile_pendencias = ProfilePendencia.objects.filter(profile=profile).filter(nivel=nivel)
+    if len(profile_pendencias) < len(pendencias):
+        ProfilePendencia.add_pendencias(profile, pendencias)
+        profile_pendencias = ProfilePendencia.objects.filter(profile=profile).filter(nivel=nivel) 
     return render(
         request, 
-        'pendencias/user_pending.html', 
+        'pendencias/pendencia_usuarios.html', 
         {
+            'usuario': user,
             'pendencias': pendencias, 
             'profile_pendencias': profile_pendencias
         }
--- members/models/profile_pendencia.py
+++ members/models/profile_pendencia.py
@@ -12,19 +12,37 @@ STATUS_PENDENCIA = (
 )
 
 class ProfilePendencia(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
     pendencia = models.ForeignKey(Pendencia, on_delete=models.DO_NOTHING)
     nivel = models.ForeignKey(Nivel, default=1, on_delete=models.DO_NOTHING)
     pendencia_status = models.IntegerField(choices=STATUS_PENDENCIA, default=0)
     updated_at = models.DateTimeField(default=datetime.now)
 
+    def add_pendencias(profile, pendencias):
+        user_pendencias = ProfilePendencia.objects.filter(profile=profile).filter(nivel=pendencia.nivel)
+        for pendencia in pendencias:
+            if pendencia in user_pendencias:
+                continue
+            prof_pendencia = ProfilePendencia()
+            prof_pendencia.profile = profile
+            prof_pendencia.pendencia = pendencia
+            prof_pendencia.nivel = pendencia.nivel
+            prof_pendencia.save()
+
+    def update_pendencia_status(status, profile_pendencia):
+        profile_pendencia.pendencia_status = status
+        profile_pendencia.save()        
+
     def get_pendencias(profile: Profile):
         if profile.nivel_id is None:
             profile.nivel_id = Nivel.objects.get(nivel=profile.nivel)
+        if profile.nivel.lower() == 'diretor':
+            return []
         prx_nivel = profile.nivel_id.proximo_nivel()
         pendencias = Pendencia.objects.filter(nivel=prx_nivel)
-        pendencias_done = ProfilePendencia.objects.filter(profile=profile)
+        pendencias_done = ProfilePendencia.objects.filter(profile=profile).filter(pendencia_status=2)
+        pendencias_done = [ p.pendencia for p in pendencias_done]
         pendencias_faltantes = []
         if pendencias is None:
             return []
