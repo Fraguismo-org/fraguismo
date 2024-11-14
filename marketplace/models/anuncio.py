48d6bc2bf9674f7e8f138c8518eeed9b9025b502
Paulo Figueiredo <figueiredo@linkmetrik.com>
2024-11-14 05:45:04 -0300

    marketplace: criado cards

--- marketplace/models/anuncio.py
+++ marketplace/models/anuncio.py
@@ -9,6 +9,21 @@ STATUS_ANUNCIO = (
     (3, 'Pausado'),
 )
 
+DEPARTAMENTO = (
+    (0, 'Outros'),
+    (1, 'Agro e indústria'),
+    (2, 'Alimetação'),
+    (3, 'Autos e peças'),
+    (4, 'Eletrônicos e celulares'),
+    (5, 'Esportes e lazer'),
+    (6, 'Imóveis'),
+    (7, 'Moda beleza'),
+    (8, 'Músicas e hobbies'),
+    (9, 'Para a sua casa'),
+    (10, 'Prestação de serviços'),
+    (11, 'Vagas de emprego'),
+)
+
 class Anuncio(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,6 +31,7 @@ class Anuncio(models.Model):
     titulo = models.CharField(max_length=50, null=False)
     descricao = models.TextField(max_length=4096, null=False)    
     status_anuncio = models.IntegerField(choices=STATUS_ANUNCIO, default=1)
+    departamento = models.IntegerField(choices=DEPARTAMENTO, default=0)
     preco = models.DecimalField(decimal_places=2, null=False, default=0.00, max_digits=15)
     created_at = models.DateTimeField(default=datetime.now, null=False)
     updated_at = models.DateTimeField(default=datetime.now, null=True)
