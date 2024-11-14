3a22ce969011167f49b36297d95526bf78368d4c
Paulo Figueiredo <figueiredo@linkmetrik.com>
2024-11-14 08:27:44 -0300

    site-fraguismo: index

--- site_fraguismo/urls.py
+++ site_fraguismo/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('codigo_conduta/', views.codigo_conduta, name='codigo_conduta'),
     path('termos_condicoes/', views.termos_condicoes, name='termos_condicoes'),
+    path('sobre_nos/', views.sobre_nos, name='sobre_nos'),
+    path('contato/', views.contato, name='contato'),
 ]
