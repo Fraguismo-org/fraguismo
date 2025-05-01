d5ada7af10a143c9521664a22a7a3876c2ed09ce
Paulo <admin@linkmetrik.com>
2025-04-30 21:09:45 -0300

    pvfigueiredo feat: Mudança nomes dos cargos

--- fraguismo/fixtures/niveis.sql
+++ fraguismo/fixtures/niveis.sql
@@ -4,3 +4,10 @@ REPLACE INTO `rating_nivel` (`id`, `nivel`, `pontuacao_base`) VALUES
     (3, 'lider', 15),
     (4, 'executivo', 30),
     (5, 'diretor', 40);
+
+REPLACE INTO `rating_nivel` (`id`, `nivel`, `pontuacao_base`) VALUES
+    (1, 'aprendiz', 0),
+    (2, 'escudeiro', 5),
+    (3, 'cavaleiro', 15),
+    (4, 'conselheiro', 30),
+    (5, 'guardião', 40);
