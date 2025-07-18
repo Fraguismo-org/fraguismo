83f4cc14319989f2540c83880afb8d74defee6ca
TheoTavora <theovannitavora@gmail.com>
2025-07-18 20:19:11 -0300

    ADD: Models proposta CREATE: Form

--- propostas/models.py
+++ propostas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Proposta(models.Model):
+    titulo = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    arquivo = models.FileField(upload_to='propostas/')
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
