bd52a5a51e47026102673e7c2fa85cae9f3d23c0
TheoTavora <theovannitavora@gmail.com>
2025-07-22 19:23:18 -0300

    cria tabela de votos no banco de dados

--- propostas/models.py
+++ propostas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Proposta(models.Model):
     titulo = models.CharField(max_length=255)
@@ -9,3 +10,12 @@ class Proposta(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+class Voto(models.Model):
+    proposta = models.ForeignKey('Proposta', on_delete=models.CASCADE, related_name='votos')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    votos = models.BooleanField()  # True = a favor, False = contra
+    data = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('proposta', 'usuario')
