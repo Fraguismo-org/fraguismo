4a947a09257833920aaf75d82bcc54bbeba69906
TheoTavora <theovannitavora@gmail.com>
2025-07-22 19:23:43 -0300

    Caminho de votação

--- propostas/urls.py
+++ propostas/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('propostas/', views.create_proposal, name='create_proposal'),
     path('listar_propostas/', views.show_proposal, name='show_proposal'),
     path('details_proposal/<int:proposta_id>/', views.details_proposal, name='details_proposal'),
+    path('votar/<int:proposta_id>/', views.vote_proposal, name='vote_proposal'),
 ]
