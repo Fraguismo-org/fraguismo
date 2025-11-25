4c724285512d1778d75b984a4d43a84a8f777045
TheoTavora <theovannitavora@gmail.com>
2025-11-25 20:55:55 -0300

    navegação para adms

--- administrador/urls.py
+++ administrador/urls.py
@@ -3,9 +3,14 @@ from administrador.views import (
     administrador_views,
     pendencias_administrador_views,
 )
+from members.views import user_view
+
 
 
 urlpatterns = [
     path('adm/', view=administrador_views.administrador, name='adm'),    
     path('pendencia_usuarios/<str:username>', view=pendencias_administrador_views.pendencia_usuario, name='pendencia_usuarios'),
+    path('user_page/', user_view.user_page, name='user_page'),
+    path('users/', user_view.lista_usuarios, name='lista_usuarios'),
+    path('navigation/', view=administrador_views.navigation, name='navigation'),
 ]
